1038565329cfc31e91265a100695d58eab27400f
hge2ne <hge2ne@gmail.com>
2025-06-02 01:58:27 +0900

    useRouter

--- app/list/page.js
+++ app/list/page.js
@@ -1,23 +1,20 @@
-import{connectDB} from "@/util/database.js"
+import {connectDB} from "@/util/database.js"
+import Link from "next/link"
 
 export default async function List() {
     let db = (await connectDB).db('next-practice');//db연결
     let result = await db.collection('post').find().toArray();
-     return (
+    return (
 
         <div className="list-bg">
-            <div className="list-item">
-                <h4>{result[0].title}</h4>
-                <p>1월 1일</p>
-            </div>
-            <div className="list-item">
-                <h4>{result[1].title}</h4>
-                <p>1월 1일</p>
-            </div>
-            <div className="list-item">
-                <h4>{result[2].title}</h4>
-                <p>1월 1일</p>
-            </div>
-        </div>
-    )
-}
+            {
+                result.map((a, i) =>
+                    <div className="list-item" key={i}>
+                        <Link href={'/detail/' + result[i]._id}><h4>{a.title}</h4>링크</Link>
+                        < p> 1월 1일</p>
+                    </div>
+                )
+            }
+                </div>
+                )}
+
